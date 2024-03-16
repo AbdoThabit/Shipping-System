a0aa48ddc904f3117ceacf4840007eacad23043b
mohamedpasyony <mohamedpasyony41@gmail.com>
2024-03-16 02:51:34 +0200

    Code First

---
+++ Shipping System/DAL/Database/Context.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shipping_System.DAL.Entites;
+
+namespace Shipping_System.DAL.Database
+{
+    public class Context : IdentityDbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+          
+        }
+
+        public virtual DbSet<Branch> Branches { get; set; }
+        public virtual DbSet<Governate> Governates { get; set; }
+        public virtual DbSet<City> Cities { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ShippingSetting> ShippingSettings { get; set; }
+        public virtual DbSet<WeightSetting> WeightSettings{ get ; set; }
+        public virtual DbSet<VillageShipping>  VillageSettings { get; set; }
+
+
+
+
+    }
+}
+
