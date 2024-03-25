5fa1122354d174d8ea0e9cf8e848a4dd6bf4ed17
Abdelrahman Thabit <abdelrahman4ever98@gmail.com>
2024-03-25 19:27:14 +0200

    village repository done!

---
+++ Shipping System/ViewModels/VillageSettingVM.cs
@@ -0,0 +1,14 @@
+﻿using Shipping_System.DAL.Entites;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shipping_System.ViewModels
+{
+    public class VillageSettingVM
+    {
+        public int? Id { get; set; }
+
+        [Required(ErrorMessage = "  سعر الشــحن للقريـة مطلوب")]
+        public decimal Price { get; set; }
+    }
+}
