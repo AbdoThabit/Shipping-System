eedd7450f17eb34efd5166a158b97e9541a827e5
Abdelrahman Thabit <abdelrahman4ever98@gmail.com>
2024-03-25 19:06:45 +0200

    all about weight settings (done!)

--- Shipping System/Controllers/WeightSettingsController.cs
+++ Shipping System/Controllers/WeightSettingsController.cs
@@ -40,5 +40,47 @@ namespace Shipping_System.Controllers
 
             return View(weightSettingsVM);
         }
+        public async Task<IActionResult> Update(int Id)
+        {
+            var WeightSetting = await _WeightSettingsRepo.GetById(Id);
+            return View(WeightSetting);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(WeightSettingsVM WeightSetting)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _WeightSettingsRepo.Edit(WeightSetting);
+                if (result != 0)
+                {
+                    _ToastNotification.AddSuccessToastMessage("تم تعديل نوع الشحن بنجاح");
+
+                    return RedirectToAction("Index");
+
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Failed to update . Please try again.");
+                    return View(WeightSetting);
+                }
+            }
+            return View(WeightSetting);
+
+        }
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var result = await _WeightSettingsRepo.Delete(Id);
+            if (result != 0)
+            {
+                _ToastNotification.AddSuccessToastMessage("تم حذف نوع الشحن بنجاح");
+
+                return Ok();
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Failed to delete . Please try again.");
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
